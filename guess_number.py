5d3b5ae125a860f5f1668166c56c1240a806e84e
MatFernandex <mat.fernandex@gmail.com>
2024-04-02 15:58:03 -0300

    att

---
+++ guess_number.py
@@ -0,0 +1,19 @@
+import random
+
+print("Welcome to the Guess")
+
+random_number = random.randint(1, 100)
+attempts = 10
+response = 0
+while int(response) != random_number:
+    response = input("Guess a number between 1 and 100: ")
+    if int(response) ==random_number:
+        print("Correct!")
+    elif int(response) > random_number:
+        attempts -= 1
+        print("you you missed, the number is smaller. You still have {}".format(attempts))
+    elif int(response) < random_number:
+        attempts -= 1
+        print("you you missed, the number is bigger. You still have {}".format(attempts))
+
+print("Thank you for playing!")
